3dacdcd422f787c5ccb041b86f194cacdba742b5
Thomas Manceau <manceau.thomas19@gmail.com>
2017-05-23 18:04:00 +0200

    Vulkan wrapper interface added with some declarations: 	- Initialize() 	- Uninitialize()

---
+++ includes/pil/gw/vulkan.h
@@ -0,0 +1,11 @@
+#ifndef __BK_VULKAN_H__
+#define __BK_VULKAN_H__
+
+#include <vulkan/vulkan.h>
+
+// ~~~~~ Dcl(INTERNAL | PUBLIC) ~~~~~
+
+extern void	Initialize(void);
+extern void	Uninitialize(void);
+
+#endif
---
+++ sources/pil/gw/vulkan.c
@@ -0,0 +1,18 @@
+#include "../../../includes/pil/gw/vulkan.h"
+
+static VkInstance	__vkInstance = NULL;
+
+// ~~~~~ Dcl(INTERNAL | PRIVATE) ~~~~~
+
+
+// ~~~~~ Def(ALL) ~~~~~
+
+void	Initialize(void)
+{
+
+}
+
+void	Uninitialize(void)
+{
+
+}
