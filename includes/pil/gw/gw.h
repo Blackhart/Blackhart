293a1e03be72c8e09c06e5d6e5fa8c690d0eea21
Thomas Manceau <manceau.thomas19@gmail.com>
2017-05-23 19:32:42 +0200

    Vulkan Initialization and Unitialization done

--- includes/pil/gw/gw.h
+++ includes/pil/gw/gw.h
@@ -1,9 +1,11 @@
 #ifndef __BK_GW_H__
 #define __BK_GW_H__
 
+#include "../adt.h"
+
 // ~~~~~ Dcl(INTERNAL | PUBLIC) ~~~~~
 
-extern void	GwInitialize();
+extern void	GwInitialize(char const* const pExtensions, uint8 const pExtensionCount);
 extern void	GwUninitialize();
 
 #endif
--- sources/pil/gw/vulkan.c
+++ sources/pil/gw/vulkan.c
@@ -1,31 +1,44 @@
-#include "gw.h"
+#include "../includes/pil/gw/gw.h"
 #include "../../../includes/pil/gw/vulkan.h"
 
 static VkInstance	__vkInstance = NULL;
 
 // ~~~~~ Dcl(INTERNAL | PRIVATE) ~~~~~
 
-extern void	Initialize_VkApplicationInfo(VkApplicationInfo*);
+extern void	Initialize_VkApplicationInfo(VkApplicationInfo* const);
+extern void	Initialize_VkInstanceCreateInfo(VkInstanceCreateInfo* const, VkApplicationInfo const* const, char const* const pExtensions, uint8 const pExtensionCount);
 
 // ~~~~~ Def(ALL) ~~~~~
 
-void	GwInitialize(void)
+void	GwInitialize(char const* const pExtensions, uint8 const pExtensionCount)
 {
-	VkApplicationInfo lInfo;
-	Initialize_VkApplicationInfo(&lInfo);
+	VkApplicationInfo lAppInfo;
+	VkInstanceCreateInfo lInstInfo;
+	Initialize_VkApplicationInfo(&lAppInfo);
+	Initialize_VkInstanceCreateInfo(&lInstInfo, &lAppInfo, pExtensions, pExtensionCount);
+	VkResult lResult = vkCreateInstance(&lInstInfo, NULL, &__vkInstance);
+	if (lResult != VK_SUCCESS)
+		return;
 }
 
 void	GwUninitialize(void)
 {
-
+	vkDestroyInstance(__vkInstance, NULL);
 }
 
-void	Initialize_VkApplicationInfo(VkApplicationInfo* ptr)
+void	Initialize_VkApplicationInfo(VkApplicationInfo* const ptr)
 {
 	ptr->sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	ptr->pApplicationName = "";
-	ptr->applicationVersion = VK_MAKE_VERSION(0, 1, 0);
 	ptr->pEngineName = "Blackhart Engine | Vulkan Renderer";
 	ptr->engineVersion = VK_MAKE_VERSION(0, 1, 0);
 	ptr->apiVersion = VK_API_VERSION_1_0;
+}
+
+void	Initialize_VkInstanceCreateInfo(VkInstanceCreateInfo* const ptr, VkApplicationInfo const* const pAppInfo, char const* const pExtensions, uint8 const pExtensionCount)
+{
+	ptr->sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	ptr->pApplicationInfo = pAppInfo;
+	ptr->enabledExtensionCount = pExtensionCount;
+	ptr->ppEnabledExtensionNames = pExtensions;
+	ptr->enabledLayerCount = 0;
 }
