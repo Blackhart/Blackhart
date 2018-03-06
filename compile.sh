1c9d1a781983767cbcca803db3bc941546d3d947
Thomas Manceau <thomas.manceau@navya.tech>
2018-03-06 13:34:57 +0100

    Add INSTALL target
    Install target give us the possibility to compile and pack all the needed stuff
    (static libs, shared libs and includes) in a "ready-to-copy" folder.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 rm -rf build
 mkdir build
 cd build
-cmake -G"Visual Studio 14 2015" -DBUILD_BLACKHART_TESTS:BOOL=true -DUNITY_LIBRARY_PATH:PATH="extlibs/unity/lib" -DUNITY_INCLUDE_PATH:PATH="extlibs/unity/include"  ..
+cmake -G"Visual Studio 14 2015 Win64" -DBUILD_BLACKHART_TESTS:BOOL=true -DUNITY_LIBRARY_PATH:PATH="extlibs/unity/lib" -DUNITY_INCLUDE_PATH:PATH="extlibs/unity/include" ..
 cd ..
