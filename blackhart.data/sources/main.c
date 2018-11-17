1696655b643b8d253f81f5c74ea22403c50658bd
Blackhart <manceau.thomas19@gmail.com>
2018-11-17 07:47:06 +0100

    Improve data generation speed

--- blackhart.data/sources/main.c
+++ blackhart.data/sources/main.c
@@ -6,11 +6,12 @@
 #include <time.h>
 #include <stdio.h>
 #include <string.h>
+#include <omp.h>
 
 // ~~~~~ Constantes ~~~~~
 
 static int const    RIGHT_ASCENSION_MIN = 0;
-static int const    RIGHT_ASCENSION_MAX = 359; // because 360 degrees = 0 degrees
+static int const    RIGHT_ASCENSION_MAX = 360;
 static int const    DECLINAISON_MIN = -90;
 static int const    DECLINAISON_MAX = 90;
 static int const    DISTANCE_MIN = 0;
@@ -20,12 +21,14 @@ static int const    STARS_PER_SOLID_ANGLE = 1700000000 / (360 * 180);
 // ~~~~~ Dcl(INTERNAL) ~~~~~
 
 static void generate_data(void);
-static void serialize(void* buffer, size_t buffer_length, int ra, int dec);
 
 // ~~~~~ Def(PUBLIC) ~~~~~
 
 int main(void)
 {
+    // initialize omp
+    omp_set_num_threads(omp_get_max_threads());
+
     // initialize random seed
     srand((unsigned int)time(NULL));
 
@@ -39,42 +42,46 @@ int main(void)
 
 void    generate_data(void)
 {
-    size_t buffer_length = sizeof(size_t) + STARS_PER_SOLID_ANGLE * 3 * sizeof(double);
-    void* buffer = malloc(buffer_length * sizeof(char));
-    if (buffer == NULL)
+    char filename[100];
+    memset(filename, '\0', 100);
+    sprintf(filename, "%s/Stars", BK_DEFAULT_DATA_PATH);
+
+    FILE* flux = fopen(filename, "w");
+    if (flux == NULL)
         return;
 
-    ((size_t*)buffer)[0] = STARS_PER_SOLID_ANGLE;
+    size_t ra1_dec1_length = sizeof(size_t) + STARS_PER_SOLID_ANGLE * 3 * sizeof(double);
+    size_t ra1_dec180_length = ra1_dec1_length * 180;
 
-    double* coords = (double*)(((char*)buffer) + sizeof(size_t));
+    void* buffer = malloc(ra1_dec180_length);
+    if (buffer == NULL)
+        goto CLOSE_FILE;
 
-    for (int right_ascension = RIGHT_ASCENSION_MIN; right_ascension <= RIGHT_ASCENSION_MAX; ++right_ascension)
+    for (int right_ascension = RIGHT_ASCENSION_MIN; right_ascension < RIGHT_ASCENSION_MAX; ++right_ascension)
     {
-        for (int declinaison = DECLINAISON_MIN; declinaison <= DECLINAISON_MAX; ++declinaison)
+        for (int declinaison = DECLINAISON_MIN; declinaison < DECLINAISON_MAX; ++declinaison)
         {
-            for (int star_count = 0; star_count < STARS_PER_SOLID_ANGLE; ++star_count)
+            void* offseted_buffer = (char*)buffer + ra1_dec1_length * (declinaison + 90);
+
+            ((size_t*)offseted_buffer)[0] = STARS_PER_SOLID_ANGLE;
+
+            double* coords = (double*)(((char*)offseted_buffer) + sizeof(size_t));
+
+            for (int star_count = 0; star_count < STARS_PER_SOLID_ANGLE * 3; star_count += 3)
             {
                 // generate random RA, Dec, Dist
                 coords[star_count] = (rand() / (double)RAND_MAX) * ((right_ascension + 1) - right_ascension) + right_ascension;
                 coords[star_count + 1] = (rand() / (double)RAND_MAX) * ((declinaison + 1) - declinaison) + declinaison;
                 coords[star_count + 2] = (rand() / (double)RAND_MAX) * (DISTANCE_MAX - DISTANCE_MIN) + DISTANCE_MIN;
             }
-
-            // serialize stars coords on the disk
-            serialize(buffer, buffer_length, right_ascension, declinaison);
         }
+
+        // serialize stars coords on the disk
+        fwrite(buffer, sizeof(char), ra1_dec180_length, flux);
     }
 
     free(buffer);
-}
 
-void    serialize(void* buffer, size_t buffer_length, int ra, int dec)
-{
-    char filename[100];
-    memset(filename, '\0', 100);
-    sprintf(filename, "%s/Stars_%d_%d", BK_DEFAULT_DATA_PATH, ra, dec);
-
-    FILE* flux = fopen(filename, "w");
-    fwrite(buffer, sizeof(char), buffer_length, flux);
+CLOSE_FILE:
     fclose(flux);
 }
--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 rm -rf build
 mkdir build
 cd build
-cmake -G"Visual Studio 15 2017 Win64" -DBUILD_BLACKHART_TESTS:BOOL=true -DBUILD_BLACKHART_STUDIO_TESTS:BOOL=false -DUNITY_LIBRARY_PATH:PATH="../../extlibs/unity/lib" -DUNITY_INCLUDE_PATH:PATH="../../extlibs/unity/include" -DGLEW_LIBRARY_PATH:PATH="../extlibs/glew/lib" -DGLEW_INCLUDE_PATH:PATH="../extlibs/glew/include" -DGLFW_LIBRARY_PATH:PATH="../extlibs/glfw/lib" -DGLFW_INCLUDE_PATH:PATH="../extlibs/glfw/include" -DGTEST_LIBRARY_PATH:PATH="../../extlibs/googletest/googletest/lib" -DGTEST_INCLUDE_PATH:PATH="../../extlibs/googletest/googletest/include" -DQT_DIRECTORY_PATH:PATH="D:/Program/Qt/5.10.1/5.10.1/msvc2015_64" ..
+cmake -G"Visual Studio 14 2015 Win64" -DBUILD_BLACKHART_TESTS:BOOL=true -DBUILD_BLACKHART_STUDIO_TESTS:BOOL=false -DUNITY_LIBRARY_PATH:PATH="../../extlibs/unity/lib" -DUNITY_INCLUDE_PATH:PATH="../../extlibs/unity/include" -DGLEW_LIBRARY_PATH:PATH="../extlibs/glew/lib" -DGLEW_INCLUDE_PATH:PATH="../extlibs/glew/include" -DGLFW_LIBRARY_PATH:PATH="../extlibs/glfw/lib" -DGLFW_INCLUDE_PATH:PATH="../extlibs/glfw/include" -DGTEST_LIBRARY_PATH:PATH="../../extlibs/googletest/googletest/lib" -DGTEST_INCLUDE_PATH:PATH="../../extlibs/googletest/googletest/include" -DQT_DIRECTORY_PATH:PATH="D:/Program/Qt/5.10.1/5.10.1/msvc2015_64" ..
 cd ..
